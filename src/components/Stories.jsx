6abbb2e78af6dd2012e7ae09b8c3359617b6537e
buraxta <burakgulluler@gmail.com>
2023-09-08 14:01:20 +0300

    style the stroy section

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -17,7 +17,10 @@ const Stories = () => {
   }, []);
 
   return (
-    <div>
+    <div
+      className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 
+    border-1 overflow-x-scroll rounded-sm scrollbar-none"
+    >
       {storyUsers.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
