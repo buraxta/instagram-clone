cf27e96510f9a22d3778e30e044153d8b22a264e
buraxta <burakgulluler@gmail.com>
2023-09-11 13:33:59 +0300

    make the feed section responsive

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,12 +4,12 @@ import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         <Stories />
         <Posts />
       </section>
-      <section></section>
+      <section className="md:col-span-1 hidden"></section>
     </main>
   );
 };
